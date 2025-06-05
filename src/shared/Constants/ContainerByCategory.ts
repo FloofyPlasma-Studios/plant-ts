3b9e0199439d85230f98f81433267170ec113e59
riall <duelisms@gmail.com>
2025-06-05 05:37:11 -0400

    feat: ContainerByCategory work

--- src/shared/Constants/ContainerByCategory.ts
+++ src/shared/Constants/ContainerByCategory.ts
@@ -1,4 +1,18 @@
-/**
- * A mapping of ItemCategory to their container so that item lookups can
- * be performed by item category and itemId
- */
+/*
+	A mapping of ItemCategory to their container so that item lookups can
+	be performed by item category and itemId
+*/
+
+import { ReplicatedStorage } from "@rbxts/services";
+
+import { ItemCategory } from "./ItemCategory";
+import { getInstance } from "shared/Utility/getInstance";
+
+export const ContainerByCategory: { [key in ItemCategory]: Folder } = {
+	[ItemCategory.Plants]: getInstance<Folder>(ReplicatedStorage, "Instances", "Plants"),
+	[ItemCategory.Pots]: getInstance<Folder>(ReplicatedStorage, "Instances", "Pots"),
+	[ItemCategory.Seeds]: getInstance<Folder>(ReplicatedStorage, "Instances", "Seeds"),
+	[ItemCategory.Wagons]: getInstance<Folder>(ReplicatedStorage, "Instances", "Wagons"),
+	[ItemCategory.Tables]: getInstance<Folder>(ReplicatedStorage, "Instances", "Tables"),
+	[ItemCategory.CoinBundles]: getInstance<Folder>(ReplicatedStorage, "Instances", "CoinBundles"),
+};
